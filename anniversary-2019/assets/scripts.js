3d15db572f4d90072e395ee78ba62eaa24688937
Garvit Shrestha <shrestha.garvit@wensolutions.com>
2020-03-01 13:26:00 +0545

    changes

--- anniversary-2019/assets/scripts.js
+++ anniversary-2019/assets/scripts.js
@@ -1,41 +1,111 @@
-jQuery(document).ready((function ($) {
-    $(".btn-display-main-content").click((function (e) {
-        e.preventDefault(), $("body").addClass("body-class"), $(".main-content-wrapper").fadeIn(2e3), $(".initial-display").remove()
-    }));
-    var targetDate = new Date("2019/12/1 00:00:00"),
-        days, hrs, min, sec;
+jQuery(document).ready(function ($) {
+    $('.btn-display-main-content').click(function (e) {
+        e.preventDefault();
+        $('body').addClass('body-class');
+        $('.main-content-wrapper').fadeIn(2000);
+        $('.initial-display').remove();
+    });
 
-    function timeToLaunch() {
-        var currentDate, diff = (new Date - targetDate) / 1e3,
-            diff = Math.abs(Math.floor(diff));
-        days = Math.floor(diff / 86400), sec = diff - 24 * days * 60 * 60, hrs = Math.floor(sec / 3600), sec -= 60 * hrs * 60, min = Math.floor(sec / 60), sec -= 60 * min
+    // VANTA.BIRDS({
+    //     el: ".initial-display"
+    // })
+
+    /* --------------------------
+     * GLOBAL VARS
+     * -------------------------- */
+    // The date you want to count down to
+    var targetDate = new Date("2019/12/1 00:00:00");
+
+    // Other date related variables
+    var days;
+    var hrs;
+    var min;
+    var sec;
+
+    /* --------------------------
+     * ON DOCUMENT LOAD
+     * -------------------------- */
+    $(function() {
+       // Calculate time until launch date
+       timeToLaunch();
+      // Transition the current countdown from 0
+      numberTransition('#days .number', days, 1000, 'easeOutQuad');
+      numberTransition('#hours .number', hrs, 1000, 'easeOutQuad');
+      numberTransition('#minutes .number', min, 1000, 'easeOutQuad');
+      numberTransition('#seconds .number', sec, 1000, 'easeOutQuad');
+      // Begin Countdown
+      setTimeout(countDownTimer,1001);
+    });
+
+    /* --------------------------
+     * FIGURE OUT THE AMOUNT OF
+       TIME LEFT BEFORE LAUNCH
+     * -------------------------- */
+    function timeToLaunch(){
+        // Get the current date
+        var currentDate = new Date();
+
+        // Find the difference between dates
+        var diff = (currentDate - targetDate)/1000;
+        var diff = Math.abs(Math.floor(diff));
+
+        // Check number of days until target
+        days = Math.floor(diff/(24*60*60));
+        sec = diff - days * 24*60*60;
+
+        // Check number of hours until target
+        hrs = Math.floor(sec/(60*60));
+        sec = sec - hrs * 60*60;
+
+        // Check number of minutes until target
+        min = Math.floor(sec/(60));
+        sec = sec - min * 60;
+
     }
 
-    function countDownTimer() {
-        timeToLaunch(), $("#days .number").text(days), $("#hours .number").text(hrs), $("#minutes .number").text(min), $("#seconds .number").text(sec), 0 == days && 0 == hrs && 0 == min && 0 == sec && ($("ul#countdown").remove(), $(".container").removeAttr("style")), setTimeout(countDownTimer, 1e3)
+    /* --------------------------
+     * DISPLAY THE CURRENT
+       COUNT TO LAUNCH
+     * -------------------------- */
+    function countDownTimer(){
+
+        // Figure out the time to launch
+        timeToLaunch();
+
+        // Write to countdown component
+        $( "#days .number" ).text(days);
+        $( "#hours .number" ).text(hrs);
+        $( "#minutes .number" ).text(min);
+        $( "#seconds .number" ).text(sec);
+        if ( days >= 0 ) {
+          $('ul#countdown').remove();
+          $('.container').removeAttr('style');
+        }
+
+        // Repeat the check every second
+        setTimeout(countDownTimer,1000);
     }
 
-    function numberTransition(id, endPoint, transitionDuration, transitionEase) {
-        $({
-            numberCount: $(id).text()
-        }).animate({
-            numberCount: endPoint
-        }, {
-            duration: transitionDuration,
-            easing: transitionEase,
-            step: function () {
-                $(id).text(Math.floor(this.numberCount))
-            },
-            complete: function () {
-                $(id).text(this.numberCount)
-            }
-        })
-    }
-    $((function () {
-        timeToLaunch(), numberTransition("#days .number", days, 1e3, "easeOutQuad"), numberTransition("#hours .number", hrs, 1e3, "easeOutQuad"), numberTransition("#minutes .number", min, 1e3, "easeOutQuad"), numberTransition("#seconds .number", sec, 1e3, "easeOutQuad"), setTimeout(countDownTimer, 1001)
-    })), $(".select-btn-heart-false").click((function (e) {
-        e.preventDefault(), alert("Sorry dear... This is not the right one.")
-    })), $(".select-btn-heart-true").click((function (e) {
-        e.preventDefault(), alert("Yeah.... ✨✨✨ You got it. Now sit back and enjoy this song, our favourite song; 😁"), $("#youtube-player-wrapper").removeAttr("style"), $("#closing-section").removeAttr("style")
-    }))
-}));
+    /* --------------------------
+     * TRANSITION NUMBERS FROM 0
+       TO CURRENT TIME UNTIL LAUNCH
+     * -------------------------- */
+    function numberTransition(id, endPoint, transitionDuration, transitionEase){
+      // Transition numbers from 0 to the final number
+      $({numberCount: $(id).text()}).animate({numberCount: endPoint}, {
+          duration: transitionDuration,
+          easing:transitionEase,
+          step: function() {
+            $(id).text(Math.floor(this.numberCount));
+          },
+          complete: function() {
+            $(id).text(this.numberCount);
+          }
+       });
+    };
+
+    $('.select-btn-heart').click(function (e) {
+      e.preventDefault();
+      alert( 'Thank you for clicking 😜. Mobile maa camera cha, herara hello bhandinu 😜.' );
+    });
+});
